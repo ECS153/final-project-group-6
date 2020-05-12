b7b9cd0c7e6240559376520013514c7d524e5e1a
Jadewhy <huiyumm@gmail.com>
2020-05-12 11:53:45 -0700

    create a file

---
+++ speakEZ/speakEZ/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  speakEZ
+//
+//  Created by Jade on 5/12/20.
+//  Copyright © 2020 Jade. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
