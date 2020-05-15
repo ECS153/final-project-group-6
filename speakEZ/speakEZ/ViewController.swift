83cf7998a9fa60185a76c0ddeba54552917b39e3
Brian Nguyen <hbrnguyen@ucdavis.edu>
2020-05-14 17:59:10 -0700

    chatbox skeleton
    able to send messages

--- speakEZ/speakEZ/ViewController.swift
+++ speakEZ/speakEZ/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	@IBOutlet weak var chatTable: UITableView!
+	@IBOutlet weak var textField: UITextField!
+	@IBAction func sendButton() {
+		self.mytextMessages.append(self.textField.text ?? "")
+		self.textField.text = ""
+		numCells += 1
+		self.chatTable.reloadData()
+		
+		
+	}
+	
+	var numCells = 0
+	var mytextMessages = [String]()
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		self.chatTable.dataSource = self
+		self.chatTable.delegate = self
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return numCells
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "textCell") ?? UITableViewCell(style: .default, reuseIdentifier: "textCell")
+		cell.textLabel?.text = ""
+		cell.detailTextLabel?.text = self.mytextMessages[indexPath.row]
+
+		cell.textLabel?.numberOfLines = 0
+		return cell
 	}
 
 
